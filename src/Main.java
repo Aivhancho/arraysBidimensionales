ed3c5fb2fee9880bf99d3f2b1ab9e959292c2c27
ivan <falso@falso.es>
2024-03-22 13:17:50 +0100

    apuntes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //al final se hace primero filas y luego columnas (5 filas(arrays) y 4 columnas(datos))
+        int numeros[][] = new int[/*5*/][/*4*/]{{1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
+        Scanner sc = new Scanner(System.in);
+        //int edades[][] = {{1,3,4},{1,2,5}};
+        for(int i=0; i < 5; i++){//eje X
+            //se repetirá el for de debajo hasta que el eje X sea 4
+            for(int j=0; j < 4; j++){//eje Y
+                System.out.println("Introduce los números");
+                numeros[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+}
